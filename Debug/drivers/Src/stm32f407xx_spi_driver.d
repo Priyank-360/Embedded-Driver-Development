6b74531259195c89c18d67d8de20194d6c433992
Ghatge <P.Ghatge@astrofein.com>
2020-09-10 15:46:01 +0200

    SPI Driver: send data

--- Debug/drivers/Src/stm32f407xx_spi_driver.d
+++ Debug/drivers/Src/stm32f407xx_spi_driver.d
@@ -1,11 +1,11 @@
-drivers/Src/stm32f407xx_gpio_driver.o: \
- ../drivers/Src/stm32f407xx_gpio_driver.c \
- C:/workspace_stm/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
+drivers/Src/stm32f407xx_spi_driver.o: \
+ ../drivers/Src/stm32f407xx_spi_driver.c \
+ C:/workspace_stm/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h \
  C:/workspace_stm/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
  C:/workspace_stm/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
  C:/workspace_stm/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h
 
-C:/workspace_stm/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
+C:/workspace_stm/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h:
 
 C:/workspace_stm/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
 
